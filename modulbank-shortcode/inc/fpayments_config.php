1f4d01ce57e2c6f27824b1e44d9b3aa355c68e43
Yuriy Vasilyev <rus.xprogrammer@gmail.com>
2019-03-07 14:58:40 +0700

    Update fpayments_config.php

--- modulbank-shortcode/inc/fpayments_config.php
+++ modulbank-shortcode/inc/fpayments_config.php
@@ -1,7 +1,6 @@
 <?php
 
 class FPaymentsSCConfig {
-    //const HOST = 'http://172.21.11.3';
     const HOST = 'https://pay.modulbank.ru';
     const NAME = 'Modulbank';
     const PREFIX = 'modulbank_sc';
